a3e2a7438b8ea0471ddee72a5ab9aa8a09b8f37e
Travus <helmlyw@gmail.com>
2015-08-02 01:00:01 -0400

    added a function to change button image

--- button.js
+++ button.js
@@ -1,19 +1,29 @@
 /*function to create a button.*/
 
 function createButton(buttonNumber, imgsrc){
-	var buttonID = 'button ' + buttonNumber;
+	var buttonName = 'button ' + buttonNumber;
 	
 	//create an image HTML element for the button
 	var newButton = document.createElement("img");
-	newButton.setAttribute('id',buttonID);
-	newButton.setAttribute('alt', buttonID);
+	newButton.setAttribute('name',buttonName);
+	newButton.setAttribute('alt', buttonName);
 	newButton.setAttribute('src', imgsrc);
 	
 	//temporary ID and will need to be fixed in both HTML and JS
 	var buttonTag = document.getElementById("button");
 	buttonTag.appendChild(newButton);
 	alert('End of Function');
+	return newButton;
+}
+
+/*function to change the image for various mouse events. */
+
+function changeImage()
+{
+	//just test code to make sure it works
+	document.images['button 1'].src = "blue_button.jpg";
 }
 
 
-createButton(1, 'image1.png');
+var button1 = createButton(1, 'image1.png');
+changeImage();
