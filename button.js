fad6880b1f6ec0db6d63c8b70576405723245bee
Travus <helmlyw@gmail.com>
2015-08-02 00:43:08 -0400

    createButton() now places a button image to HTML file

--- button.js
+++ button.js
@@ -7,13 +7,13 @@ function createButton(buttonNumber, imgsrc){
 	var newButton = document.createElement("img");
 	newButton.setAttribute('id',buttonID);
 	newButton.setAttribute('alt', buttonID);
+	newButton.setAttribute('src', imgsrc);
 	
 	//temporary ID and will need to be fixed in both HTML and JS
 	var buttonTag = document.getElementById("button");
-	buttonTag.innerHTML = "a";
 	buttonTag.appendChild(newButton);
 	alert('End of Function');
 }
 
 
-createButton(1);
+createButton(1, 'image1.png');
