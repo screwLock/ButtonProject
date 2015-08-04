3de44b36bef7dfd6276b8789973cf7a35873bb2c
Travus <helmlyw@gmail.com>
2015-08-03 21:02:06 -0400

    deleted clickable function, and instead added wrapInAnchor function to new file called helmlyUtility.js to hold useful js functions

--- button.js
+++ button.js
@@ -1,23 +1,58 @@
-/*function to create a button.*/
+/*****************************************
+Function: createButton()
+Purpose:  Creates a button.  The button is
+		  is basically an image tag.  
+		  
+Parameters: buttonNumber:  the number for the
+			button's name. String
+			imgsrc:  the image for the button,
+			should be a string giving the 
+			location of the image.
+			
+Output:  returns the button after appending it to
+		  the html document.
+******************************************/
 
 function createButton(buttonNumber, imgsrc){
 	var buttonName = 'button ' + buttonNumber;
 	
 	//create an image HTML element for the button
 	var newButton = document.createElement("img");
+	
+	//Bad Code Smell--Should be refactored
 	newButton.setAttribute('name',buttonName);
 	newButton.setAttribute('alt', buttonName);
 	newButton.setAttribute('src', imgsrc);
 	
+	//newButton.setAttribute('onMouseOver', "return changeImage()");
+	
+	
 	//temporary ID and will need to be fixed in both HTML and JS
 	var buttonTag = document.getElementById("button");
 	buttonTag.appendChild(newButton);
-	alert('End of Function');
+	//alert('End of Function');
 	return newButton;
 }
 
-/*function to change the image for various mouse events. */
 
+/*function to make a button clickable--
+--wraps image in an anchor tag--*/
+
+function makeClickable(clickButton)
+{
+	//make anchor tags <a></a> parent of button element
+}
+
+
+
+
+/*******************************************************
+Function: changeImage()
+Purpose: to change the image for various mouse events. 
+Parameters:  oldButton:  button node object to be changed
+			 newImagesrc:  string specifying the file
+			 location of the new button image to be used
+*******************************************************/
 function changeImage(oldButton, newImagesrc)
 {
 	var oldImageName = oldButton.getAttribute("name");
@@ -27,6 +62,11 @@ function changeImage(oldButton, newImagesrc)
 }
 
 
+
+
 var button1 = createButton(1, 'image1.png');
-changeImage(button1, "blue_button.jpg");
-changeImage(button1, "image1.png")
+//var button2 = createButton(2, 'image1.png');
+
+wrapInAnchor('google.com', button1);
+//changeImage(button1, "blue_button.jpg");
+//changeImage(button1, "image1.png")
---
+++ helmlyUtility.js
@@ -0,0 +1,29 @@
+/******************************************
+function:  element wrapName
+Purpose:  wrap an html element in an anchor tag
+		  homemade jQuery wrap()
+		  !!!!NOTE, this will wrap the element AFTER it has been placed in html!!!!!!
+		  
+Parameters: hyperlink = destination hyper link in String format
+			wrapped = element you want wrapped by its name.
+
+********************************************/
+
+function wrapInAnchor(hyperlink, wrapped){
+	//create the wrapper anchor tag
+	var wrapper = document.createElement('a');
+	wrapper.setAttribute('href',hyperlink);
+	
+	//get the unwrapped element and its parent
+	var unwrappedElement = document.getElementsByName(wrapped.getAttribute("name"));
+	var parent = wrapped.parentNode;
+	
+	
+	wrapper.appendChild(unwrappedElement[0]);
+	parent.appendChild(wrapper);
+	
+	alert(wrapper.outerHTML);
+	alert('finished');
+	
+}
+
